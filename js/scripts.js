8f20306101bf538cb7165ce611164989a00472c7
Esti Shay and Shyamal Punekar <esti@>
2017-07-18 13:53:04 -0700

    add logic to handle all form inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,50 @@
 //business logic
+function Ticket(movie, time, age) {
+  this.movie = movie;
+  this.time = time;
+  this.age = age;
+  this.price = 15;
+}
 
+Ticket.prototype.movieRequest = function() {
+  return this.movie + ", " + this.time + ", " + this.age;
+}
+
+Ticket.prototype.calcPrice = function() {
+  if (this.movie === "Casablanca") {
+    this.price = this.price - 12;
+  } else if (this.age === "adult" && this.time > 4) { //,maybe hav more logic?
+    this.price = this.price;
+  } else if (this.age === "child" && this.time > 4) {
+    this.price = this.price - 7;
+  } else if (this.age === "senior" && this.time > 4) {
+    this.price = this.price - 7;
+  } else if (this.age === "adult" && this.time <= 4) {
+    this.price = this.price - 7;
+  } else if (this.age === "child" && this.time <= 4) {
+    this.price = this.price - 10;
+  } else if (this.age === "senior" && this.time <= 4) {
+    this.price = this.price - 10;
+  }
+}
 
 //UI logic
 $(document).ready(function() {
   $("#get-movie-prices").submit(function(event) {
-
     var showInput = $("#movie-titles").val();
-    var timeInput = $("#showtimes").val();
-    var ageInput = $("input:radio[name=age-group]:checked").val();
+    var timeInput = parseInt($("#showtimes").val());
+    var ageInput = $("input:radio[name=ageGroup]:checked").val();
+    //  debugger;
 
-    $("ul").append("<li>" + showInput + "</li>");
-    $("ul").append("<li>" + timeInput + "</li>");
-    $("ul").append("<li>" + ageInput + "</li>");
+    var newTicket = new Ticket(showInput, timeInput, ageInput);
+
+    newTicket.calcPrice();
+    //var actualPrice = calcPrice(showInput, timeInput, ageInput);
+    // $("ul").append("<li>" + showInput + "</li>");
+    // $("ul").append("<li>" + timeInput + "</li>");
+    // $("ul").append("<li>" + ageInput + "</li>");
+    // $("ul").append("<li>" + newRequest.calcPrice() + "</li>")
+    $(".show-price").text("$" + newTicket.price);
     $(".prices").show();
 
     event.preventDefault();
